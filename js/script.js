008bfbd7ff6eb2590542e0d324949d3321cfcf88
Chicco80 <chicco80lisa@gmail.com>
2022-03-28 17:11:06 +0200

    modifiche

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@
 
   let ripetizioni = 100;
 
- for ( let i = 1; i <=100; i++){
-// console.log(i);
+   for ( let i = 1; i <=100; i++){
+     
+   console.log(i);
   }
 
 
@@ -16,26 +17,48 @@
    for ( let i = 1; i <=100; i++){
 
     if(i % 3 == 0)
-   console.log(i);
+    console.log(i);
 
  }
+  const app = document.getElementById("app");
+  const fizz = document.createElement("div");
+  fizz.setAttribute("class","div-3");
+  app.append(fizz);
+  console.log(fizz)
+  fizz.innerHTML=  "fizz";
+
+
  //dividi per 5
 
    for ( let i = 1; i <=100; i++){
 
     if(i % 5 == 0)
-   console.log(i);
+    console.log(i);
+
 
  }
+  const secondo = document.getElementById("secondo");
+  const buzz = document.createElement("div");
+  buzz.setAttribute("class","div-5");
+  app.append(buzz);
+  console.log(buzz)
+  buzz.innerHTML=  "buzz";
 
 // divido per 3 e 5
 
 for ( let i = 1; i <=100; i++){
 
    if ((i % 3 == 0) && ( i % 3 == 0))
-   console.log(i);
+    console.log(i);
+
 
 }
+const terzo = document.getElementById("terzo");
+  const fizzbuzz = document.createElement("div");
+  fizzbuzz.setAttribute("class","div-5-3");
+  app.append(fizzbuzz);
+  console.log(fizzbuzz)
+  fizzbuzz.innerHTML=  "fizzbuzz";
 
 
 
