c3a9b4157ea5ff19a08efec4e7c4b6d14b957916
Chicco80 <chicco80lisa@gmail.com>
2022-03-28 16:09:08 +0200

    ok calcolo

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+
+//stampo da 1 a 100 in console
+
+  let ripetizioni = 100;
+
+ for ( let i = 1; i <=100; i++){
+// console.log(i);
+  }
+
+
+ //stampo i multipli di 3
+
+ 
+
+   for ( let i = 1; i <=100; i++){
+
+    if(i % 3 == 0)
+   console.log(i);
+
+ }
+ //dividi per 5
+
+   for ( let i = 1; i <=100; i++){
+
+    if(i % 5 == 0)
+   console.log(i);
+
+ }
+
+// divido per 3 e 5
+
+for ( let i = 1; i <=100; i++){
+
+   if ((i % 3 == 0) && ( i % 3 == 0))
+   console.log(i);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+    
+
