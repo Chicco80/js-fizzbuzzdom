2a9b925206b41fcfd1dd84151ef0eea950eea2c3
Chicco80 <chicco80lisa@gmail.com>
2022-03-29 10:47:37 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,49 +1,68 @@
 
+//      <div class="row">
+//      <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+//          <div class="box">
+//              pippo
+//          </div>
 
+//      </div>
+//  </div>
 
-
+const app = document.getElementById("app");
+const row = document.createElement("div");
+row.setAttribute("class", "row");// la metto fuori perche e una sola
 
 
 
   let ripetizioni = 100;
 
    for ( let i = 1; i <=100; i++){
+// creo colonna con classi
+      const col = document.createElement("div");
+      col.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2 gy-4");
+
+      // creo box con classi
+      const box = document.createElement("div");
+      box.setAttribute("class","box");
+      let valore;
+
+      
 
       if ((i % 3 == 0) && ( i % 5 == 0)){
       console.log("fizzbuzz");
-      document.write( "fizzbuzz") 
+      valore = "fizzbuzz";
+    
+     
+      
 
    } else if (i % 3 == 0){
-      console.log("fizz")
-      document.write( "fizz") 
+      console.log("fizz");
+      valore = "fizz";
 
    } else if ( i % 5 == 0){
       console.log("buzz");
+      valore = "buzz";
 
 
 
       
    } else {
       console.log (i);
-      document.write(i)
+      valore = i;
 
       
 
 
    }
-    
+   box.innerHTML = valore;
+   box.classList.add(valore);
+    col.append(box);
+    row.append(col);
 }
-    
+    app.append(row);
 
 
 
-
-//  const terzo = document.getElementById("terzo");
-//   const fizzbuzz = document.createElement("div");
-//   fizzbuzz.setAttribute("class","div-5-3");
-//   app.append(fizzbuzz);
-//   console.log(fizzbuzz)
-//   fizzbuzz.innerHTML=  "fizzbuzz";
 
 
 
