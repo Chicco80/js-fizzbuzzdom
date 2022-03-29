63a6bddb35868630893cceb1b054d66cda4311ab
Chicco80 <chicco80lisa@gmail.com>
2022-03-29 07:49:13 +0200

    modifica

--- js/script.js
+++ js/script.js
@@ -9,10 +9,7 @@
 
    for ( let i = 1; i <=100; i++){
 
-      
-
-
-      if ((i % 3 == 0) && ( i % 3 == 0)){
+      if ((i % 3 == 0) && ( i % 5 == 0)){
       console.log("fizzbuzz");
       document.write( "fizzbuzz") 
 
@@ -22,11 +19,15 @@
 
    } else if ( i % 5 == 0){
       console.log("buzz");
+
+
+
+      
    } else {
       console.log (i);
       document.write(i)
 
-      // document.write(i ++) + "fizzbuzz"
+      
 
 
    }
